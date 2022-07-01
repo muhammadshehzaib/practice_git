85748af0aab5cd0f402cfa8dd9d100c98854d5d2
muhammadshehzaib <shehzaibbutt222@gmail.com>
2022-07-01 04:48:10 -0700

    playing with the new branch

--- index.js
+++ index.js
@@ -3,8 +3,3 @@ let newNumber = 12;
 numberStore = [...numberStore, newNumber, ...numberStore];
 console.log(numberStore);
 
-a = "i am a good boy"
-console.log(a);
-
-b = "lahore is a good city"
-console.log(b);
