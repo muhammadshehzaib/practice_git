38666001edc44cb325abfccda59aa2394d2abd2d
muhammadshehzaib <shehzaibbutt222@gmail.com>
2022-07-01 04:58:11 -0700

    for loop

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ let newNumber = 12;
 numberStore = [...numberStore, newNumber, ...numberStore];
 console.log(numberStore);
 
+// program to display text 5 times
+const n = 5;
+
+// looping from i = 1 to 5
+for (let i = 1; i <= n; i++) {
+    console.log(`I love JavaScript.`);
+}
