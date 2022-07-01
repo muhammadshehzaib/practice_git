58f5a2f288da9e1ff00d6b26f7827e6cb0c9f4e8
muhammadshehzaib <shehzaibbutt222@gmail.com>
2022-07-01 04:59:42 -0700

    another for loop

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ const n = 5;
 for (let i = 1; i <= n; i++) {
     console.log(`I love JavaScript.`);
 }
+// program to display numbers from 1 to 5
+n = 5;
+
+// looping from i = 1 to 5
+// in each iteration, i is increased by 1
+for (let i = 1; i <= n; i++) {
+    console.log(i);     // printing the value of i
+}
